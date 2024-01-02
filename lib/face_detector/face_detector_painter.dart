4932cfb4914ebb066c582171428ee4e768fc8692
whenyourapprun <whenyourapprun@gmail.com>
2024-01-02 12:16:56 +0900

    얼굴탐지 테두리 모서리 두껍게 다시 선으로 그리고 있음

--- lib/face_detector/face_detector_painter.dart
+++ lib/face_detector/face_detector_painter.dart
@@ -15,13 +15,32 @@ class FaceDetectorPainter extends CustomPainter {
   final Size imageSize;
   final InputImageRotation rotation;
   final CameraLensDirection cameraLensDirection;
+  final Color lineColor = Colors.deepPurpleAccent;
+  final double lineWidth = 6.0;
 
   @override
   void paint(Canvas canvas, Size size) {
     final Paint paint1 = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = 3.0
+      ..strokeWidth = 1.0
       ..color = Colors.yellow; // 얼굴 테두리 색상
+
+    final Paint paint2 = Paint()
+      ..color = lineColor
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = lineWidth;
+    final Paint paint3 = Paint()
+      ..color = lineColor
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = lineWidth;
+    final Paint paint4 = Paint()
+      ..color = lineColor
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = lineWidth;
+    final Paint paint5 = Paint()
+      ..color = lineColor
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = lineWidth;
 
     for (final Face face in faces) {
       final left = translateX(
@@ -52,11 +71,26 @@ class FaceDetectorPainter extends CustomPainter {
         rotation,
         cameraLensDirection,
       );
-
       canvas.drawRect(
         Rect.fromLTRB(left, top, right, bottom),
         paint1,
       );
+
+      var width = right - left;
+      var delta = width * 0.2;
+      Offset p21 = Offset(left, top);
+      Offset p22 = Offset(left + delta, top);
+      Offset p31 = Offset(left, top);
+      Offset p32 = Offset(left, top - delta);
+      Offset p41 = Offset(left, bottom);
+      Offset p42 = Offset(left + delta, bottom);
+      Offset p51 = Offset(left, bottom);
+      Offset p52 = Offset(left, bottom + delta);
+
+      canvas.drawLine(p21, p22, paint2);
+      canvas.drawLine(p31, p32, paint3);
+      canvas.drawLine(p41, p42, paint4);
+      canvas.drawLine(p51, p52, paint5);
     }
   }
 
