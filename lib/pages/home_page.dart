0be7287a750dd16ece04e253e0eba9ac218a4ae5
whenyourapprun <whenyourapprun@gmail.com>
2024-01-06 17:44:14 +0900

    메메인화면 변경

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,7 +22,7 @@ class HomePage extends StatelessWidget {
       body: Stack(
         children: [
           Image.asset(
-            'assets/images/elf_bg_4.png',
+            'assets/images/elf_bg_3.png',
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
