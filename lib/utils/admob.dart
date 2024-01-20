1cae755464bf698f3d7af74591b943eeebdac2bc
whenyourapprun <whenyourapprun@gmail.com>
2024-01-21 05:40:17 +0900

    라이브러리 최신 및 안드로이드 애드몹 설정

--- lib/utils/admob.dart
+++ lib/utils/admob.dart
@@ -1,9 +1,4 @@
-const Map<String, String> banner = {
-  'ios': 'ca-app-pub-4377563560880478/6466351400',
-  'android': 'ca-app-pub-4377563560880478/6829243368',
-};
-
 const Map<String, String> rewarded = {
   'ios': 'ca-app-pub-4377563560880478/3418064976',
-  'android': 'ca-app-pub-4377563560880478/7299925386',
+  'android': 'ca-app-pub-4377563560880478/8142343152',
 };
